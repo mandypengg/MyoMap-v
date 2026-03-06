867d1ddc1be8ce8e35424d67773daa8667e71b65
Mandy Peng <mandypeng2007work@gmail.com>
2026-03-06 18:38:04 -0500

    added sidebar

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ setTimeout(() => {
     }, 1000);
 
 }, 1350);
+
+const menuBtn = document.getElementById('menuBtn');
+const sidebar = document.getElementById('sidebar');
+
+menuBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('open');
+
+  if (sidebar.classList.contains('open')) {
+    menuBtn.style.display = 'none';
+  } else {
+    menuBtn.style.display = 'block';
+  }
+
+});
+
+const closeBtn = document.getElementById('closeBtn');
+
+closeBtn.addEventListener('click', () => {
+  sidebar.classList.remove('open');
+  menuBtn.style.display = 'block';
+});
